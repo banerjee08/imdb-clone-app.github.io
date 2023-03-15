71708477fda548cc2fcfbe28c0ad5cc108cc7920
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2023-03-15 21:22:39 +0530

    Favourite and movie page done

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-// fetch('http://www.omdbapi.com/?t=batman&apikey=56d596e9')
-//     .then(res => res.json())
-//     .then(data => console.log(data))
+fetch('http://www.omdbapi.com/?t=batman&apikey=56d596e9')
+    .then(res => res.json())
+    .then(data => console.log(data))
