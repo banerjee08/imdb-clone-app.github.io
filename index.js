d52a19fb0991d1b7916762c96bba1913b806d748
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2023-03-17 00:39:08 +0530

    search box added

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-fetch('http://www.omdbapi.com/?t=batman&apikey=56d596e9')
-    .then(res => res.json())
-    .then(data => console.log(data))
+const searchInput = document.getElementById('search-input')
+const searchList = document.getElementById('search-list')
+console.log(searchList)
+
+// search.addEventListener('input', filterList)
+
+// function filterList() {
+//     let searchInput = search.value.toLowerCase()
+//     console.log(searchInput)
+
+    
+// }
+
+
+// const searchMovie = () => {
+//     let filter = document.getElementById('myInput').value.toUpperCase();
+    
+    
+//     fetch(`http://www.omdbapi.com/?t=${filter}&apikey=56d596e9`)
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+// }
+// fetch(`http://www.omdbapi.com/?t=batman&apikey=56d596e9`)
+// .then(res => res.json())
+// .then(data => console.log(data))
