bceb17d1354f7361f0533bfe21c093701ff0aeb0
Sumanto Banerjee <68064874+banerjee08@users.noreply.github.com>
2023-03-17 03:02:26 +0530

    search list showing results as we type

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
-const searchInput = document.getElementById('search-input')
-const searchList = document.getElementById('search-list')
-console.log(searchList)
+const searchInput = document.getElementById('search-input');
+const searchList = document.getElementById('search-list');
 
-// search.addEventListener('input', filterList)
+async function loadMovies(searchTerm) {
+  await fetch(`http://www.omdbapi.com/?t=${searchTerm}&page=1&apikey=56d596e9`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      displayMovieList(data);
+      //   if ((data.Response = 'True')) {
+      //   }
+    });
+}
 
-// function filterList() {
-//     let searchInput = search.value.toLowerCase()
-//     console.log(searchInput)
+// loadMovies('batman')
 
-    
-// }
+function findMovies() {
+  let searchTerm = searchInput.value.trim();
+  if (searchTerm.length > 0) {
+    searchList.classList.remove('hide-search-list');
+    loadMovies(searchTerm);
+  } else {
+    searchList.classList.add('hide-search-list');
+  }
+}
 
+let html = '';
 
-// const searchMovie = () => {
-//     let filter = document.getElementById('myInput').value.toUpperCase();
-    
-    
-//     fetch(`http://www.omdbapi.com/?t=${filter}&apikey=56d596e9`)
-//     .then(res => res.json())
-//     .then(data => console.log(data))
-// }
-// fetch(`http://www.omdbapi.com/?t=batman&apikey=56d596e9`)
-// .then(res => res.json())
-// .then(data => console.log(data))
+function displayMovieList(movie) {
+  searchList.innerHTML = '';
+  if (movie.Poster != 'N/A') {
+    moviePoster = movie.Poster;
+  } else {
+    moviePoster = './photos/image_not_found.png';
+  }
+
+    if (movie.Response != "False") {
+        html += `
+        <div class="search-list-item ">
+        <div class="search-item-thumbnail">
+        <img src="${moviePoster}" alt="Poster of ${movie.Title}">
+        </div>
+        <div class="search-item-info">
+        <div>
+        <h4>${movie.Title}</h4>
+        <p>${movie.Year}</p>
+        </div>
+        <div class="add-to-favourites-btn">
+        <p>
+        Add to favorites
+        <i class="fa-regular fa-star"></i>
+        </p>
+        </div>
+        </div>
+        </div>
+        `;
+    }
+    searchList.innerHTML = html;
+}
